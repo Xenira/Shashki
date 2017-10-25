5c67cb980e47e633366e823208b5f3fa7a474595
Lasse Sprengel <lasse.sprengel@protonmail.ch>
2017-10-25 14:24:29 +0200

    Added basic app structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+import { GameService } from './game.service';
+import { BaseRouterModule } from './base-router/base-router.module';
+import { MaterialModule } from './material/material.module';
+import { PieceComponent } from './piece/piece.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    PieceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    BaseRouterModule,
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/game.service.ts
+++ frontend/src/app/game.service.ts
@@ -1,8 +1,40 @@
+/// <reference path="../../node_modules/shashki-logic/dist/game.d.ts" />
+
 import { Injectable } from '@angular/core';
+import Game from 'shashki-logic';
+import { Piece, Color } from 'shashki-logic';
 
 @Injectable()
 export class GameService {
 
+  game = new Game();
+  playerColor: Color = Color.LIGHT;
+
+  beats: {[key: string]: boolean} = {};
+
   constructor() { }
+
+  getBoard() {
+    return this.game.board;
+  }
+
+  getMoves(x: number, y: number, piece: Piece) {
+    if (this.playerColor !== piece.color || !this.isMyTurn()) {
+      return { moves: [], beats: [] };
+    }
+    const moves = this.game.getPossibleMoves(x, y, piece.color, piece.king);
+    if (moves.beats.length || moves.moves.length) {
+      console.log(x, y, piece, moves);
+    }
+    return moves;
+  }
+
+  isMyTurn() {
+    return this.game.currentPlayer === this.playerColor;
+  }
+
+  canBeat() {
+
+  }
 
 }
