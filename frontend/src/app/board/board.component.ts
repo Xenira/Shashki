24d14ee3e3c173b1d783fac04c9f10945c9f9e4e
Lasse Sprengel <lasse.sprengel@protonmail.ch>
2017-10-25 14:22:40 +0200

    Added game board

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GameService } from '../game.service';
+import { Piece, Color } from 'shashki-logic';
 
 @Component({
   selector: 'app-board',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  @Input() gameId: string;
+  @Input() color: Color;
+
+  board: Piece[][];
+
+  constructor(private _game: GameService) { }
 
   ngOnInit() {
+    this.board = this._game.getBoard();
+  }
+
+  getMoves(x: number, y: number, piece: Piece) {
+    return this._game.getMoves(x, y, piece);
   }
 
 }
