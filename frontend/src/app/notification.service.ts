c5e51428e0c22082eb516c35050a47d67236a935
Lasse Sprengel <lasse.sprengel@protonmail.ch>
2017-10-28 10:51:10 +0200

    Added notification service

---
+++ frontend/src/app/notification.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+@Injectable()
+export class NotificationService {
+
+  constructor(private _snackbar: MatSnackBar) { }
+
+  showNotification(message: string) {
+    this._snackbar.open(message);
+  }
+}
